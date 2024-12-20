f9554ff28aa4a3c88642382fb98819ed035a777f
Nabilabintaakter <nabilabintaakter@gmail.com>
2024-12-20 13:23:50 +0600

    implemented routes to fetch job listings: /jobs (limit to 6) and /allJobs (fetch all)

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5f9uk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,12 +25,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
-
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+// jobs related apis
+
+    const jobsCollection = client.db('jobPortal').collection('jobs');
+
+    // get 6 jobs in the homepage
+    app.get('/jobs', async(req,res)=>{
+        const result = await jobsCollection.find().limit(6).toArray();
+        res.send(result);
+    })
+    // get all jobs
+    app.get('/allJobs', async(req,res)=>{
+        const result = await jobsCollection.find().toArray();
+        res.send(result);
+    })
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
